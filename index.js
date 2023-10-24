fd97320cada5d8c1b8d68b24e8f6550ace75271b
ngiad <follownghia001@gmail.com>
2023-10-24 23:19:35 +0700

    test

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express"
+
+
+const app = express()
+
+//  request yeu cau tu phia client 
+//  response phan hoi tu phia server
+app.get("/api/get",(request,response) => {
+    // response.send("phan hoi send : chi dc phep gui string")
+    //response.send  gui 1 string
+
+    //  khi muon gui du lieu la kieu tham chieu (object,array,function)
+    response.json({data : {name : "nghia"}})
+})
+
+
+
+app.listen(5000,() => {
+    console.log("server is running on PORT : "+ 5000);
+})
